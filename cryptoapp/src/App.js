ba41abca9aa96271127738d732928f6d37f57c6c
Naeem <developerflk@gmail.com>
2024-09-18 04:57:23 +0500

    Updated FIles

--- cryptoapp/src/App.js
+++ cryptoapp/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
-
+import Header from './components/Global/Header';
+import Footer from './components/Global/Footer';
 function App() {
   return (
-    <div className="App">
-    
+    <div className="app">
+      <Header />
+      <div className='bodySection'></div>
+      <Footer />
     </div>
   );
 }
---
+++ cryptoapp/src/components/Global/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footerSection'>Footer</footer>
+  )
+}
+
+export default Footer
